e04405a8d9ab81a3ed0fef7cd34523ae2ee0ac13
chrisdalao <chrisdalao@gmail.com>
2019-05-28 02:13:06 -0700

    Social Card completed MVP and Stretch

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return(
+        <div className="footer">
+        <i className="far fa-comment"></i>
+        <i className="fas fa-sync-alt"></i>6 
+        <i className="far fa-heart"></i>4 
+        <i className="far fa-envelope"></i>
+        </div>
+    );
+};
+
+export default Footer;
